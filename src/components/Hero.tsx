f30bf42e917abd34eb88b10cee921d2b4e7a1013
Airel Adrivano <165012306+theoneandonlyvabo@users.noreply.github.com>
2026-06-26 19:52:52 +0700

    aasdjla

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,7 @@
 // src/components/Hero.tsx
 
 export default function Hero() {
-
   return (
-
     <section style={{
       display: 'flex',
       flexDirection: 'column',
@@ -13,47 +11,61 @@ export default function Hero() {
       gap: '12px',
     }}>
 
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        gap: 'var(--space-sm)',
+        marginTop: '100px',
+      }}>
+        <img
+          src="/src/assets/aireladrivano.jpeg"
+          alt="aireladrivano"
+          style={{
+            width: '65px',
+            height: '65px',
+            borderRadius: '5px',
+            objectFit: 'cover',
+          }}
+        />
         <h1 style={{
-            fontFamily: 'var(--font-main)',
-            fontSize: 'var(--text-xl)',
-            fontWeight: '700',
-            marginTop: '100px',
-            letterSpacing: '-0.05em',
-            color: 'var(--text)',
+          fontFamily: 'var(--font-main)',
+          fontSize: 'var(--text-xl)',
+          fontWeight: '700',
+          letterSpacing: '-0.05em',
+          color: 'var(--text)',
         }}>
-            aireladrivano
+          aireladrivano
         </h1>
+      </div>
 
-        <p style={{
-            fontFamily: 'var(--font-body)',
-            fontSize: 'var(--text-lg)',
-            color: 'var(--text-muted)',
-        }}>
-            Innovator
-        </p>
+      <p style={{
+        fontFamily: 'var(--font-body)',
+        fontSize: 'var(--text-lg)',
+        color: 'var(--text-muted)',
+      }}>
+        Innovator
+      </p>
 
-        <div style={{
-            fontFamily: 'var(--font-body)',
-            marginTop: '100px',
-            fontSize: 'var(--text-md)',
-            color: 'var(--text)',
-            animation: 'breathe 2s ease-in-out infinite',
+      <div style={{
+        fontFamily: 'var(--font-body)',
+        marginTop: '100px',
+        fontSize: 'var(--text-md)',
+        color: 'var(--text)',
+        animation: 'breathe 2s ease-in-out infinite',
+      }}>
+        Scroll Down ↓
+      </div>
 
-        }}>
-            Scroll Down ↓
-        </div>
-
-        <div style={{
-            fontFamily: 'var(--font-body)',
-            marginTop: '100px',
-            fontSize: 'var(--text-md)',
-            color: 'var(--text)',
-
-        }}>
-            Architecting products from zero to one. End-to-end
-            <br></br>
-            execution with a founder's eye for value.
-        </div>
+      <div style={{
+        fontFamily: 'var(--font-body)',
+        marginTop: '100px',
+        fontSize: 'var(--text-md)',
+        color: 'var(--text)',
+      }}>
+        Architecting products from zero to one. End-to-end
+        <br />
+        execution with a founder's eye for value.
+      </div>
 
     </section>
   )
